d00d0cd1628c1bfec3f772389f8bd6ce0ce5fc56
Aluno SENAI <romulo010397@gmail.com>
2025-04-22 22:01:28 -0300

    fim aula

---
+++ aula22part2.js
@@ -0,0 +1,13 @@
+function exercicio1(){
+    let num1 = Number (prompt("digite um numero"))
+    let num2 = Number (prompt("digite outro numero"))
+    let soma = num1 + num2
+    alert( `a soma dos dois numeros é: ${soma}´)
+    alert(`a soma dos dois numeros é: " + soma´)
+
+
+
+
+
+
+}
